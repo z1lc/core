7055614842def6dbe9eb3f9d0668dd3c7c160b4f
rsanek <rsanek@gmail.com>
2019-05-02 20:51:25 -0700

    ignore

--- src/test/java/com/robertsanek/data/quality/anki/AllSyntaxHighlightedCodeHasMobileFriendlyLineLengthsTest.java
+++ src/test/java/com/robertsanek/data/quality/anki/AllSyntaxHighlightedCodeHasMobileFriendlyLineLengthsTest.java
@@ -1,10 +1,12 @@
 package com.robertsanek.data.quality.anki;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class AllSyntaxHighlightedCodeHasMobileFriendlyLineLengthsTest {
 
   @Test
+  @Ignore("integration")
   public void name() {
     new AllSyntaxHighlightedCodeHasMobileFriendlyLineLengths().runDQ();
   }
