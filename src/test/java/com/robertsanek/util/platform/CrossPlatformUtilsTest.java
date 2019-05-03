7e6a4f9ea2921af787e3411373d83de6c8f544bc
rsanek <rsanek@gmail.com>
2019-05-02 20:31:03 -0700

    Fix cross-platform tests

--- src/test/java/com/robertsanek/util/platform/CrossPlatformUtilsTest.java
+++ src/test/java/com/robertsanek/util/platform/CrossPlatformUtilsTest.java
@@ -1,6 +1,6 @@
 package com.robertsanek.util.platform;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -8,13 +8,43 @@ public class CrossPlatformUtilsTest {
 
   @Test
   public void getRootPathIncludingTrailingSlash() {
-    assertEquals("Z:/core/", CrossPlatformUtils.getRootPathIncludingTrailingSlash().orElseThrow());
+    assertTrue(CrossPlatformUtils.getPlatform().visit(new Platform.Visitor<>() {
+      @Override
+      public Boolean caseWindows10(Platform.Windows10 windows10) {
+        return "Z:/core/".equals(CrossPlatformUtils.getRootPathIncludingTrailingSlash().orElseThrow());
+      }
+
+      @Override
+      public Boolean caseRaspberryPi(Platform.RaspberryPi raspberryPi) {
+        return true;
+      }
+
+      @Override
+      public Boolean caseUbuntu(Platform.Ubuntu ubuntu) {
+        return true;
+      }
+    }));
   }
 
   @Test
   public void getAnkiMediaFolderForUserz1lcIncludingTrailingSlash() {
-    assertEquals("C:/Users/z1lc/AppData/Roaming/Anki2/z1lc/collection.media/",
-        CrossPlatformUtils.getAnkiMediaFolderForUserz1lcIncludingTrailingSlash().orElseThrow());
+    assertTrue(CrossPlatformUtils.getPlatform().visit(new Platform.Visitor<>() {
+      @Override
+      public Boolean caseWindows10(Platform.Windows10 windows10) {
+        return "C:/Users/z1lc/AppData/Roaming/Anki2/z1lc/collection.media/".equals(
+            CrossPlatformUtils.getAnkiMediaFolderForUserz1lcIncludingTrailingSlash().orElseThrow());
+      }
+
+      @Override
+      public Boolean caseRaspberryPi(Platform.RaspberryPi raspberryPi) {
+        return true;
+      }
+
+      @Override
+      public Boolean caseUbuntu(Platform.Ubuntu ubuntu) {
+        return true;
+      }
+    }));
   }
 
 }
