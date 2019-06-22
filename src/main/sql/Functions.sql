c03c397180143d86d2ce494b583f0f026e1126c2
rsanek <rsanek@gmail.com>
2019-06-21 20:06:15 -0700

    Move functions to separate file

---
+++ src/main/sql/Functions.sql
@@ -0,0 +1,38 @@
+CREATE OR REPLACE FUNCTION most_recent_sunday()
+    RETURNS timestamp without time zone AS
+$$
+BEGIN
+    RETURN (select date_trunc('week', to_pst(current_timestamp) + interval '1 day') - interval '1 day');
+END;
+$$
+    LANGUAGE plpgsql
+;
+
+CREATE OR REPLACE FUNCTION day_of_week_ordinal()
+    RETURNS int AS
+$$
+BEGIN
+    RETURN (select 1 + date_part('days', to_pst(current_timestamp) - most_recent_sunday()));
+END;
+$$
+    LANGUAGE plpgsql
+;
+
+CREATE OR REPLACE FUNCTION to_pst(input timestamp without time zone)
+    RETURNS timestamp without time zone AS
+$$
+BEGIN
+    RETURN input at time zone 'utc' at time zone 'pst';
+END;
+$$
+    LANGUAGE plpgsql
+;
+CREATE OR REPLACE FUNCTION to_pst(input timestamp with time zone)
+    RETURNS timestamp without time zone AS
+$$
+BEGIN
+    RETURN input at time zone 'pst';
+END;
+$$
+    LANGUAGE plpgsql
+;
