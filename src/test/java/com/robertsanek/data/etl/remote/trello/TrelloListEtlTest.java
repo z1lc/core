21f77eecd1f71aea11a675fd7e3b31c155a636e0
rsanek <rsanek@gmail.com>
2019-06-24 11:31:14 -0700

    don't run integration test

--- src/test/java/com/robertsanek/data/etl/remote/trello/TrelloListEtlTest.java
+++ src/test/java/com/robertsanek/data/etl/remote/trello/TrelloListEtlTest.java
@@ -2,11 +2,13 @@ package com.robertsanek.data.etl.remote.trello;
 
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TrelloListEtlTest {
 
   @Test
+  @Ignore("integration")
   public void name() throws Exception {
     List<TrelloList> objects = new TrelloListEtl().getObjects();
     System.out.println("objects = " + objects);
