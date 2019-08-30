1eb106753acae6bf374a50b6735bf1460c8e7d9c
rsanek <rsanek@gmail.com>
2019-08-30 12:00:44 -0700

    Add example Google Analytics implementation

---
+++ src/main/java/com/robertsanek/data/etl/remote/google/analytics/HelloAnalytics.java
@@ -0,0 +1,170 @@
+package com.robertsanek.data.etl.remote.google.analytics;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.analyticsreporting.v4.AnalyticsReporting;
+import com.google.api.services.analyticsreporting.v4.AnalyticsReportingScopes;
+import com.google.api.services.analyticsreporting.v4.model.*;
+import com.robertsanek.util.platform.CrossPlatformUtils;
+
+/**
+ * A simple example of how to access the Google Analytics API.
+ */
+public class HelloAnalytics {
+
+  // Replace with your view ID.
+  private static final String VIEW_ID = "181777329";
+
+  // The directory where the user's credentials will be stored.
+  private static final File DATA_STORE_DIR = new File(
+      System.getProperty("user.home"), ".store/hello_analytics");
+
+  private static final String APPLICATION_NAME = "Hello Analytics Reporting";
+  private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+  private static NetHttpTransport httpTransport;
+  private static FileDataStoreFactory dataStoreFactory;
+
+  public static void main(String[] args) {
+    try {
+      AnalyticsReporting service = initializeAnalyticsReporting();
+
+      GetReportsResponse response = getReport(service);
+      printResponse(response);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Initializes an authorized Analytics Reporting service object.
+   *
+   * @return The analytics reporting service object.
+   * @throws IOException
+   * @throws GeneralSecurityException
+   */
+  private static AnalyticsReporting initializeAnalyticsReporting() throws GeneralSecurityException, IOException {
+
+    httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+    dataStoreFactory = new FileDataStoreFactory(DATA_STORE_DIR);
+
+    // Load client secrets.
+    InputStream in = new FileInputStream(
+        CrossPlatformUtils.getRootPathIncludingTrailingSlash().orElseThrow() + "in/etl/analytics_secret.json");
+    final GoogleClientSecrets clientSecrets =
+        GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in, UTF_8));
+
+    // Set up authorization code flow for all authorization scopes.
+    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow
+        .Builder(httpTransport, JSON_FACTORY, clientSecrets,
+        AnalyticsReportingScopes.all()).setDataStoreFactory(dataStoreFactory)
+        .build();
+
+    // Authorize.
+    Credential credential = new AuthorizationCodeInstalledApp(flow,
+        new LocalServerReceiver()).authorize("user");
+    // Construct the Analytics Reporting service object.
+    return new AnalyticsReporting.Builder(httpTransport, JSON_FACTORY, credential)
+        .setApplicationName(APPLICATION_NAME).build();
+  }
+
+  /**
+   * Query the Analytics Reporting API V4.
+   * Constructs a request for the sessions for the past seven days.
+   * Returns the API response.
+   *
+   * @param service
+   * @return GetReportResponse
+   * @throws IOException
+   */
+  private static GetReportsResponse getReport(AnalyticsReporting service) throws IOException {
+    // Create the DateRange object.
+    DateRange dateRange = new DateRange();
+    dateRange.setStartDate("7DaysAgo");
+    dateRange.setEndDate("today");
+
+    // Create the Metrics object.
+    Metric sessions = new Metric()
+        .setExpression("ga:sessions")
+        .setAlias("sessions");
+
+    //Create the Dimensions object.
+    Dimension browser = new Dimension()
+        .setName("ga:browser");
+
+    // Create the ReportRequest object.
+    ReportRequest request = new ReportRequest()
+        .setViewId(VIEW_ID)
+        .setDateRanges(Arrays.asList(dateRange))
+        .setDimensions(Arrays.asList(browser))
+        .setMetrics(Arrays.asList(sessions));
+
+    ArrayList<ReportRequest> requests = new ArrayList<ReportRequest>();
+    requests.add(request);
+
+    // Create the GetReportsRequest object.
+    GetReportsRequest getReport = new GetReportsRequest()
+        .setReportRequests(requests);
+
+    // Call the batchGet method.
+    GetReportsResponse response = service.reports().batchGet(getReport).execute();
+
+    // Return the response.
+    return response;
+  }
+
+  /**
+   * Parses and prints the Analytics Reporting API V4 response.
+   *
+   * @param response the Analytics Reporting API V4 response.
+   */
+  private static void printResponse(GetReportsResponse response) {
+
+    for (Report report : response.getReports()) {
+      ColumnHeader header = report.getColumnHeader();
+      List<String> dimensionHeaders = header.getDimensions();
+      List<MetricHeaderEntry> metricHeaders = header.getMetricHeader().getMetricHeaderEntries();
+      List<ReportRow> rows = report.getData().getRows();
+
+      if (rows == null) {
+        System.out.println("No data found for " + VIEW_ID);
+        return;
+      }
+
+      for (ReportRow row : rows) {
+        List<String> dimensions = row.getDimensions();
+        List<DateRangeValues> metrics = row.getMetrics();
+        for (int i = 0; i < dimensionHeaders.size() && i < dimensions.size(); i++) {
+          System.out.println(dimensionHeaders.get(i) + ": " + dimensions.get(i));
+        }
+
+        for (int j = 0; j < metrics.size(); j++) {
+          System.out.print("Date Range (" + j + "): ");
+          DateRangeValues values = metrics.get(j);
+          for (int k = 0; k < values.getValues().size() && k < metricHeaders.size(); k++) {
+            System.out.println(metricHeaders.get(k).getName() + ": " + values.getValues().get(k));
+          }
+        }
+      }
+    }
+  }
+}
