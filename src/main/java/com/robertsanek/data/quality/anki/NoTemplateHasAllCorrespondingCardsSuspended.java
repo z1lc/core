6e2f30a74dd54aa78707780766813e6efa440fe9
rsanek <rsanek@gmail.com>
2019-09-22 15:46:46 -0700

    Add exception

--- src/main/java/com/robertsanek/data/quality/anki/NoTemplateHasAllCorrespondingCardsSuspended.java
+++ src/main/java/com/robertsanek/data/quality/anki/NoTemplateHasAllCorrespondingCardsSuspended.java
@@ -17,7 +17,8 @@ public class NoTemplateHasAllCorrespondingCardsSuspended extends DataQualityBase
   @Override
   void runDQ() {
     final List<Long> ALL_SUSPENDED_TEMPLATES_ALLOWED_MODEL_IDS = Lists.newArrayList(
-        1444077615554L, //Elements template
+        1444077615554L, //Elements shared deck
+        1532358549255L, //spanish sentences shared deck
         0L
     );
     modelsInUse.stream()
