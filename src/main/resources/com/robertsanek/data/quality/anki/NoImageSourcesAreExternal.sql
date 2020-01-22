3a8dd67202ddaa46bff22ed835f6b194b0c242d2
rsanek <rsanek@gmail.com>
2020-01-21 23:20:42 -0800

    exclude spotify songs since need internet for those anyway

--- src/main/resources/com/robertsanek/data/quality/anki/NoImageSourcesAreExternal.sql
+++ src/main/resources/com/robertsanek/data/quality/anki/NoImageSourcesAreExternal.sql
@@ -1,3 +1,5 @@
 SELECT *
 FROM anki_notes
-WHERE fields LIKE '%src="http://%' OR fields LIKE '%src="https://%'
+WHERE (fields LIKE '%src="http%' OR
+        fields LIKE '%src=''http%')
+        and model_id not in (1579060616046) --spotify song
