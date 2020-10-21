41d134c34b2ed27aba81aafd15afc93c50c3f042
rsanek <rsanek@gmail.com>
2020-10-20 19:45:02 -0700

    Update time RangeMap

--- src/main/java/com/robertsanek/util/DateTimeUtils.java
+++ src/main/java/com/robertsanek/util/DateTimeUtils.java
@@ -36,6 +36,7 @@ public class DateTimeUtils {
   private static final LocalDate INDIA_UAE_2020_FEBRUARY_START = LocalDate.of(2020, 2, 28);
   private static final LocalDate INDIA_UAE_2020_MARCH_END = LocalDate.of(2020, 3, 7);
   private static final LocalDate HUNTSVILLE_2020_COVID_START = LocalDate.of(2020, 3, 17);
+  private static final LocalDate HUNTSVILLE_2020_COVID_END = LocalDate.of(2020, 10, 18);
 
   static {
     dateRangeMap.put(Range.lessThan(MOVED_TO_CA), ZoneId.of("America/Chicago")); //Huntsville, Auburn
@@ -77,7 +78,10 @@ public class DateTimeUtils {
 
     dateRangeMap.put(
         Range.closedOpen(INDIA_UAE_2020_MARCH_END, HUNTSVILLE_2020_COVID_START), ZoneId.of("America/Los_Angeles"));
-    dateRangeMap.put(Range.atLeast(HUNTSVILLE_2020_COVID_START), ZoneId.of("America/Chicago"));
+    dateRangeMap.put(
+        Range.closedOpen(HUNTSVILLE_2020_COVID_START, HUNTSVILLE_2020_COVID_END), ZoneId.of("America/Chicago"));
+
+    dateRangeMap.put(Range.atLeast(HUNTSVILLE_2020_COVID_END), ZoneId.of("America/Los_Angeles"));
   }
 
   private static ZoneId getZoneId(LocalDate localDate) {
