e36a2e8d144ef475eb69300bee20b3046f8dadef
rsanek <rsanek@gmail.com>
2021-09-01 20:52:45 -0700

    Update sleep query to use median

--- src/main/resources/com/robertsanek/sql/20210822Snapshot.sql
+++ src/main/resources/com/robertsanek/sql/20210822Snapshot.sql
@@ -1,5 +1,6 @@
 -- Queries used in The Snapshot #7 / August 2021
 -- https://docs.google.com/document/d/1-x8dtw5qWq7cDSMnkAo3Aiw4J4YjbbEvuMwt0zBN5Pc/edit#heading=h.g186q4tilmaq
+-- connect via `gcloud sql connect rsanek-db --user=postgres --quiet`
 
 -- Exercise / Cardio - target is 70%+ of days have at least 10 minutes of cardio
 select sum(case when active_zone_minutes > 10 then 1 else 0 end)::float /
@@ -15,12 +16,16 @@ with hm as (select date_of_sleep,
                    extract(hour from start_time)   as h,
                    extract(minute from start_time) as m,
                    time_in_bed
-            from fitbit_sleep)
-select avg(case when h < 12 then h + 24 else h end + m / 60) - 24 as average_bedtime,
-       avg(time_in_bed / 60.0)                                    as hours_in_bed
+            from fitbit_sleep),
+     summarized as (
+select (case when h < 12 then h + 24 else h end + m / 60) - 24 as bedtime,
+       time_in_bed / 60.0                                    as hours_in_bed
 from hm
 where date_of_sleep >= '2021-06-06'
-  and date_of_sleep < '2021-08-22';
+  and date_of_sleep < '2021-08-22')
+select percentile_cont(0.5) within group (order by bedtime) as median_bedtime,
+      percentile_cont(0.5) within group (order by hours_in_bed) as median_hours_in_bed
+from summarized;
 
 -- Books
 select title, author_name
