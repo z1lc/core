6919f66a93d49ca21bd25e2ff4e75c69a6ff0cb4
z1lc <rsanek@gmail.com>
2021-10-20 17:17:43 -0700

    don't run wikipedia

--- src/main/java/com/robertsanek/data/etl/remote/wikipedia/WikiPersonEtl.java
+++ src/main/java/com/robertsanek/data/etl/remote/wikipedia/WikiPersonEtl.java
@@ -4,12 +4,14 @@ import java.time.LocalDate;
 import java.util.List;
 
 import com.google.inject.Inject;
+import com.robertsanek.data.etl.DoNotRun;
 import com.robertsanek.data.etl.Etl;
 import com.robertsanek.data.etl.SlowEtl;
 import com.robertsanek.util.Unchecked;
 import com.robertsanek.wikipedia.WikipediaConnector;
 
 @SlowEtl
+@DoNotRun(explanation = "still works, but questionable utility (should likely be in zdone now)")
 public class WikiPersonEtl extends Etl<WikiPerson> {
 
   @Inject WikipediaConnector wikipediaConnector;
