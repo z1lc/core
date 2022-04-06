7630757c0cf569783f4a50d8051b3e3f1986eb0c
z1lc <rsanek@gmail.com>
2022-04-06 14:23:29 -0700

    fix blood pressure ETL

--- src/main/java/com/robertsanek/data/etl/remote/google/fit/BloodPressureEtl.java
+++ src/main/java/com/robertsanek/data/etl/remote/google/fit/BloodPressureEtl.java
@@ -4,6 +4,7 @@ import static com.robertsanek.util.SecretType.HEALTH_SPREADSHEET_ID;
 
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Locale;
@@ -11,6 +12,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.inject.Inject;
 import com.robertsanek.data.etl.Etl;
 import com.robertsanek.data.etl.remote.google.sheets.SheetsConnector;
@@ -21,7 +23,7 @@ import com.robertsanek.util.Unchecked;
 
 public class BloodPressureEtl extends Etl<BloodPressureReading> {
 
-  private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", new Locale("en"));
+  @VisibleForTesting static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", new Locale("en"));
   private static final AtomicLong ID_ISSUER = new AtomicLong(1);
   private static final String RANGE = "Weight & HR!A2:G10000";
 
@@ -34,7 +36,9 @@ public class BloodPressureEtl extends Etl<BloodPressureReading> {
         SheetsConnector.getSpreadsheetCells(secretProvider.getSecret(HEALTH_SPREADSHEET_ID), RANGE);
     Stream<BloodPressureReading> manual = spreadsheetCells.stream()
         .map(row -> {
-          final LocalDate date = Unchecked.get(() -> LocalDate.parse(row.get(0).toString(), DATE_FORMAT));
+          String rowDate = row.get(0).toString();
+          // really weird &nbsp; -type character getting added by Google Sheets between the date and time, we just by-hand replace it here with a regular space.
+          final LocalDate date = Unchecked.get(() -> LocalDateTime.parse(rowDate.replace(String.valueOf(rowDate.charAt(10)), " "), DATE_FORMAT)).toLocalDate();
           return BloodPressureReading.BloodPressureReadingBuilder.aBloodPressureReading()
               .withId(ID_ISSUER.getAndIncrement())
               .withDate(DateTimeUtils.toZonedDateTime(date))
