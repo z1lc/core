8384e8de70f5e59f71a22d092f88a3f4daf4c4a8
z1lc <rsanek@gmail.com>
2023-07-16 14:22:02 -0400

    don't run google analytics

--- src/main/java/com/robertsanek/data/etl/remote/google/analytics/PageViewEtl.java
+++ src/main/java/com/robertsanek/data/etl/remote/google/analytics/PageViewEtl.java
@@ -2,8 +2,10 @@ package com.robertsanek.data.etl.remote.google.analytics;
 
 import java.util.List;
 
+import com.robertsanek.data.etl.DoNotRun;
 import com.robertsanek.data.etl.Etl;
 
+@DoNotRun(explanation = "stopped working sometime in late 2022 / 2023, don't really use this anyway so decided to just turn off")
 public class PageViewEtl extends Etl<PageView> {
 
   @Override
