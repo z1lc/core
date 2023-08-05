df006a74a6e5fe1f3c208f8ce7bbe65d669cf9a9
z1lc <rsanek@gmail.com>
2023-08-04 21:53:15 -0400

    new snapshot

---
+++ src/main/resources/com/robertsanek/sql/20230805Snapshot.sql
@@ -0,0 +1,16 @@
+-- Queries used in The Snapshot #13 / May 2023
+-- https://docs.google.com/document/d/1-x8dtw5qWq7cDSMnkAo3Aiw4J4YjbbEvuMwt0zBN5Pc/edit#
+
+CREATE OR REPLACE FUNCTION weeks() RETURNS integer AS $$
+BEGIN
+    RETURN 10;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- Anki
+select sum(total_minutes) / (weeks() * 7)                                         as avg_minutes,
+       sum(case when total_minutes >= 14.5 then 1.0 else 0.0 end) / (weeks() * 7) as percent
+from rlp_daily_education
+where created_at >= '2023-05-28'
+  and created_at < '2023-08-06';
