36f58e44f7432715c2a78d8e9f9efa871a76f2d9
z1lc <rsanek@gmail.com>
2023-10-15 22:57:13 -0400

    snapshot 15 sql

--- src/main/resources/com/robertsanek/sql/20231015Snapshot.sql
+++ src/main/resources/com/robertsanek/sql/20231015Snapshot.sql
@@ -1,4 +1,4 @@
--- Queries used in The Snapshot #14 / August 2023
+-- Queries used in The Snapshot #15 / October 2023
 -- https://docs.google.com/document/d/1Y8uqNeyXJKGh8HVBX7cQtMJf0ye0HGr9RTU2Uy1cM9E
 
 CREATE OR REPLACE FUNCTION weeks() RETURNS integer AS $$
@@ -12,5 +12,5 @@ $$ LANGUAGE plpgsql;
 select sum(total_minutes) / (weeks() * 7)                                         as avg_minutes,
        sum(case when total_minutes >= 14.5 then 1.0 else 0.0 end) / (weeks() * 7) as percent
 from rlp_daily_education
-where created_at >= '2023-05-28'
-  and created_at < '2023-08-06';
+where created_at >= '2023-08-06'
+  and created_at < '2023-10-15';
