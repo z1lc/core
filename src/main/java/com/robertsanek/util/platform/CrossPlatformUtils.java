d7fd96654f2563f51f8b8128e5992acf8461da53
z1lc <rsanek@gmail.com>
2024-11-23 15:22:53 -0500

    remove

--- src/main/java/com/robertsanek/util/platform/CrossPlatformUtils.java
+++ src/main/java/com/robertsanek/util/platform/CrossPlatformUtils.java
@@ -1,11 +1,7 @@
 package com.robertsanek.util.platform;
 
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 import org.apache.commons.lang3.SystemUtils;
 
