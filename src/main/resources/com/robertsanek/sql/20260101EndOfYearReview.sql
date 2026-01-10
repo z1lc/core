fde99f83ffe2332a54535bc5dda1678b0cd578de
z1lc <rsanek@gmail.com>
2026-01-10 14:13:20 +0100

    add 2025 EOY query

---
+++ src/main/resources/com/robertsanek/sql/20260101EndOfYearReview.sql
@@ -0,0 +1,16 @@
+-- Queries used in 🔒 End-of-2025 Personal Review
+-- https://docs.google.com/document/d/1J3D11yc2KgIcw0BxHkCz93CUk-9X8oUhQtW9yg-6mB0/edit
+
+CREATE OR REPLACE FUNCTION weeks() RETURNS integer AS
+$$
+BEGIN
+    RETURN 52;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- Anki
+select sum(total_minutes) / (weeks() * 7) as avg_minutes,
+    sum(case when total_minutes >= 14.5 then 1.0 else 0.0 end) / (weeks() * 7) as percent
+from rlp_daily_education
+where created_at >= '2025-01-01' and created_at < '2026-01-01';
